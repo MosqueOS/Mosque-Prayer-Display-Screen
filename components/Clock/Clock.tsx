6d12f595580688c8f85efa88be2adc4e50791d5d
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2023-09-03 02:29:14 +0100

    Adds in Clock components

---
+++ components/Clock/Clock.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import moment from "moment";
+import { useEffect, useState } from "react"
+
+export default function Clock() {
+  const format = "h:mm A"
+  const [time, setTime] = useState(moment().format(format))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(moment().format(format))
+    }, 1000);
+
+    return () => clearInterval(interval);
+}, [format]);
+
+  return <div className="bg-white p-7 text-center">
+    <time className="text-5xl font-bold text-mosqueGreen">{time}</time>
+  </div>
+}
