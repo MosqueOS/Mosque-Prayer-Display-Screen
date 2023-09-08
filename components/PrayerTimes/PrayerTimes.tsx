b8bebd3da830f31027c571eff16d6ba8e372e45f
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2023-09-08 03:12:32 +0100

    make 12 hours

--- components/PrayerTimes/PrayerTimes.tsx
+++ components/PrayerTimes/PrayerTimes.tsx
@@ -1,4 +1,5 @@
 import { DailyPrayerTime } from "@/types/DailyPrayerTimeType"
+import moment from "moment"
 
 export default function PrayerTimes({
   today,
@@ -39,13 +40,13 @@ export default function PrayerTimes({
     <table className="text-white mx-auto table-auto border-collapse border-none w-full">
       <thead>
         <tr
-          className="text-center [&>*]:p-2 md:[&>*]:p-10
+          className="text-center [&>*]:p-2 md:[&>*]:p-8
           md:[&>*]:border [&>*]:border-mosqueGreen-dark
           [&>th]:border-t-0 [&>th:last-of-type]:border-r-0"
         >
           <th className="sr-only">Prayer time</th>
-          <th className="md:text-5xl">Begins</th>
-          <th className="md:text-5xl">Jama&apos;ah</th>
+          <th className="md:text-4xl">Begins</th>
+          <th className="md:text-4xl">Jama&apos;ah</th>
           <th className="hidden md:table-cell md:text-4xl">Tomorrow</th>
         </tr>
       </thead>
@@ -56,7 +57,7 @@ export default function PrayerTimes({
             className="
               text-center
               [&>*]:p-4
-              md:[&>*]:p-10
+              md:[&>*]:p-8
               md:[&>*]:border md:[&>*]:border-b-0 [&>*]:border-mosqueGreen-dark
               md:[&>th]:w-20
               [&>th]:border-l-0
@@ -64,15 +65,26 @@ export default function PrayerTimes({
               border border-mosqueGreen-dark border-l-0 border-r-0
               last-of-type:border-b-0"
           >
-            <th className="text-left text-2xl md:text-5xl md:text-right">
+            <th className="text-left text-2xl md:text-4xl md:text-right">
               {prayer.label}
             </th>
-            <td className="text-2xl md:text-5xl">{prayer.data.start}</td>
+            <td className="text-2xl md:text-5xl">
+              {moment(prayer.data.start, ["HH:mm"]).format("h:mm")}
+              {prayer.data?.start_secondary ? (
+                <div className="block mt-1 md:mt-2">
+                  {moment(prayer.data.start_secondary, ["HH:mm"]).format(
+                    "h:mm",
+                  )}
+                </div>
+              ) : null}
+            </td>
             <td className="font-bold text-2xl md:text-5xl ">
-              {prayer.data.congregation_start}
+              {moment(prayer.data.congregation_start, ["HH:mm"]).format("h:mm")}
             </td>
             <td className="text-2xl hidden md:table-cell md:text-5xl">
-              {prayer.tomorrow.congregation_start}
+              {moment(prayer.tomorrow.congregation_start, ["HH:mm"]).format(
+                "h:mm",
+              )}
             </td>
           </tr>
         ))}
