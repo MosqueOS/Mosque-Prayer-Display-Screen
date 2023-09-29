c38966da4dd1b4f86167c1ecbe5fcfc9ab30c88d
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2023-09-30 00:53:53 +0100

    help text to go next line for addresses

--- components/Metadata/Metadata.tsx
+++ components/Metadata/Metadata.tsx
@@ -15,7 +15,7 @@ export default function Metadata({ metadata }: { metadata: MosqueMetadata }) {
         <h2 className="mt-3 md:mt-5 font-bold text-2xl md:text-3xl">
           {metadata.name}
         </h2>
-        <p className="mt-3 text-xl">{metadata.address}</p>
+        <p className="mt-3 text-xl mx-5 md:mx-0">{metadata.address}</p>
         <p className="text-xl">{metadata.website}</p>
       </div>
     </div>
