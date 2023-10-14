2b29baa718efe531c29d065603afff3e13be55e0
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2023-10-14 03:31:04 +0100

    update manifest config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,11 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  scope: "/app",
+  sw: "sw.js",
   //disable: process.env.NODE_ENV === "development",
 })
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
-}
-
-module.exports = withPWA(nextConfig)
+})
