6d643a28f550163602d5c4901183869b564d3391
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2023-10-24 01:03:23 +0300

    default to 7 seconds

--- components/SlidingBanner/SlidingBanner.tsx
+++ components/SlidingBanner/SlidingBanner.tsx
@@ -2,12 +2,14 @@
 
 import { useEffect, useState } from "react"
 
+const transitionTime = parseInt(process.env.SLIDE_TRANSITION_TIME ?? "7") // defaults to 7 seconds
+
 export default function SlidingBanner({ slides }: { slides: any }) {
   const [currentSlide, setCurrentSlide] = useState(0)
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1)
-    }, 10 * 1000)
+    }, transitionTime * 1000)
 
     return () => clearInterval(interval)
   }, [currentSlide, slides])
