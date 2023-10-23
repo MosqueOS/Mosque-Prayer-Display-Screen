2ac2eaf0551ef164b9653d2fc05b87b18e02ebc3
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2023-10-24 00:58:19 +0300

    fix type error

--- components/UpcomingPrayerDayTiles/UpcomingPrayerDayTiles.tsx
+++ components/UpcomingPrayerDayTiles/UpcomingPrayerDayTiles.tsx
@@ -11,7 +11,9 @@ export default function UpcomingPrayerDayTiles({
       className={`grid justify-items-stretch lg:grid-cols-6 text-center gap-0 md:gap-3`}
     >
       <div className="bg-mosqueGreen-dark text-white p-4 lg:p-6 lg:col-auto">
-        <dt className="text-sm lg:text-2xl font-medium">Jama'ah times for</dt>
+        <dt className="text-sm lg:text-2xl font-medium">
+          Jama&apos;ah times for
+        </dt>
         <dd className="mt-2 text-xl lg:text-3xl font-bold tracking-tight">
           {times.display_date}
         </dd>
